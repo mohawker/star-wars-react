f5ba0d08bd8530f611906392dfd9bd6a3b0f91b2
Jun Jia <junjia2210@gmail.com>
2020-02-15 20:02:05 +0800

    Updates

---
+++ precache-manifest.34b8db231c913b211644c5f9aad604fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64664dce3862946133b321dcbaf9e7e",
+    "url": "/star-wars-react/index.html"
+  },
+  {
+    "revision": "2b05c2cab46dd73cae7d",
+    "url": "/star-wars-react/static/css/main.5c14e4da.chunk.css"
+  },
+  {
+    "revision": "efed8ba653eff2fcc5f4",
+    "url": "/star-wars-react/static/js/2.f93a32d2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/star-wars-react/static/js/2.f93a32d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b05c2cab46dd73cae7d",
+    "url": "/star-wars-react/static/js/main.c519ddbf.chunk.js"
+  },
+  {
+    "revision": "568c139a4dec694119aa",
+    "url": "/star-wars-react/static/js/runtime-main.aa4a76ce.js"
+  },
+  {
+    "revision": "d0bf0146e5c0077c7f4d6924b9790ca4",
+    "url": "/star-wars-react/static/media/BB8-loading.d0bf0146.gif"
+  },
+  {
+    "revision": "0cf7e1d82ef31810e99a733492310ca4",
+    "url": "/star-wars-react/static/media/star-wars-logo.0cf7e1d8.png"
+  }
+]);
